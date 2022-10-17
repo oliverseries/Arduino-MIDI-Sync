34b9a461030d83b9eb9f42391324e3dd45e5f3f3
aston1238 <oliver.series1998@gmail.com>
2022-10-17 19:05:05 +1100

    Add files via upload

---
+++ sketch_oct16a.ino
@@ -0,0 +1,47 @@
+int i;
+int isRunning;
+
+byte commandByte;
+
+unsigned long previousMillis = 0;
+
+void setup() {
+  i = 0;
+  isRunning = 0;
+  Serial.begin(31250);
+  // Start/stop output
+  pinMode(5, OUTPUT);
+  // Clock output
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 4 && isRunning) {
+    if (i%6 == 0) {
+      i = 0;
+      PORTD = PORTD & B00100000;
+    }
+  }
+  
+  do {
+    if (Serial.available()) {
+      commandByte = Serial.read();
+      if (commandByte == 0xFA) {
+        PORTD = PORTD | B00100000;
+        isRunning = 1;
+      }
+      if (commandByte == 0xF8 && isRunning) {
+          PORTD = B10100000;
+          previousMillis = millis();
+          i += 1;
+      }
+      if (commandByte == 0xFC) {
+        PORTD = B00000000;
+        isRunning = 0;
+        i = 0;
+      }
+    }
+  } while (Serial.available() > 2);
+}
